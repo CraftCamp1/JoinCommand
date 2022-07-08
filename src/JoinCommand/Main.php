e48b182d0620ddb51708bf160c158ee5a685a183
CraftCamp <93895629+CraftCamp1@users.noreply.github.com>
2022-07-08 14:26:29 +0800

    Create Main.php

---
+++ src/JoinCommand/Main.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace JoinCommand;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\console\ConsoleCommandSender;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\utils\Config;
+
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener {
+
+    public function onEnable() : void {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        if(!file_exists($this->getDataFolder() . "Config.yml")) {
+            $this->config = (new Config($this->getDataFolder() . "Config.yml", Config::YAML, [
+                /*Executes Command on Console */
+                "Server Command"=> "say hello {player}",
+                /*Executes Command as Player*/
+                "Player Command"=> "say hi",
+                /* Enables/Disables Commands */
+                "Server Command Enabled"=>"true",
+                "Player Command Enabled"=>"true"
+            ]));
+        } else {
+            $this->config = (new Config($this->getDataFolder() . "Config.yml", Config::YAML, []));
+        }
+    }
+
+	public function onPlayerJoin(PlayerJoinEvent $event): void {
+
+		$player = $event->getPlayer();
+		$config = new Config($this->getDataFolder() . "config.yml", Config::YAML, []);
+		$pcmd = str_replace(["{player}"], [$player->getName()], $this->getConfig()->get("Player Command"));
+		$ccmd = str_replace(["{player}"], [$player->getName()], $this->getConfig()->get("Server Command"));
+				if($config->get("Player Command Enabled") === "true"){
+	        $this->getServer()->dispatchCommand($player, $pcmd);
+	    }
+	    if($config->get("Server Command Enabled") === "true"){
+	        $this->getServer()->dispatchCommand(new ConsoleCommandSender($this->getServer(), $this->getServer()->getLanguage()), $ccmd);
+	    }
+	}
+}
